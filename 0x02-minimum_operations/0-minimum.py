adaaec15bb89135371d469e358a5d8dca30f52be
= <=>
2021-07-29 22:56:53 +0200

    implemented the interview question

---
+++ 0x02-minimum_operations/0-minimum.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Minimum operations module
+"""
+
+
+def minOperations(n):
+    """
+    execute the minimum posiible
+    """
+    if n <= 1:
+        return 0
+
+    variable1 = n
+    variable2 = 2
+    variable3 = 0
+
+    for i in range(1, variable1):
+        if variable1 % variable2 == 0:
+            variable1 = variable1 / variable2
+            variable3 = variable3 + variable2
+        else:
+            variable2 = variable2 + 1
+
+    return variable3
