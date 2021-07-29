477b5410c522effee8f7a1463adafafafce23926
= <=>
2021-07-29 23:05:16 +0200

    implemented the interview question

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -11,9 +11,7 @@ def minOperations(n):
     if n <= 1:
         return 0
 
-    variable1 = n
-    variable2 = 2
-    variable3 = 0
+    variable1, variable2, variable3 = n, 2, 0
 
     for i in range(2, variable1):
         if variable1 % variable2 == 0:
