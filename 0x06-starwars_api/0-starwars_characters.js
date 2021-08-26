cadf1f486b37c0479e4cd4ebf9e8d9b15318e068
= <=>
2021-08-26 19:56:16 +0200

    fixed bugs

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,19 @@
 #!/usr/bin/node
-
 const request = require('request');
-
 const movieId = process.argv[2];
-const url = `https://swapi.dev/api/films/${movieId}/`;
-request(url, async function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+request(url, async (error, response, body) => {
+  if (!error) {
     const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-      const res = await new Promise((resolve, reject) => {
-        request(character, (error, res, html) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(JSON.parse(html).name);
+    for (const charId in characters) {
+      await new Promise((resolve, reject) => {
+        request(characters[charId], (error, response, body) => {
+          if (!error) {
+            console.log(JSON.parse(body).name);
+            resolve();
           }
         });
       });
-      console.log(res);
     }
   }
 });
